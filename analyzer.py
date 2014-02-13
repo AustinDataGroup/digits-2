8152b137a099cfaec798c403ae6269ed3878594f
Colin <ColCarroll@users.noreply.github.com>
2014-02-13 10:56:11 -0600

    Update analyzer.py

--- analyzer.py
+++ analyzer.py
@@ -1,33 +1,28 @@
+import sys
 import numpy as np
-import csv
 from sklearn.ensemble import RandomForestClassifier
 
-csv_train_file = csv.reader(open('train.csv', 'rb'))
-csv_test_file = csv.reader(open('test.csv', 'rb'))
+TRAINFILE = 'train.csv'
+TESTFILE = 'test.csv'
+OUTFILE = 'predictions.csv'
+NESTIMATORS = 100
 
-csv_train_file.next()
-csv_test_file.next()
+def read_file(filename):
+    with open(filename, 'rb') as buff:
+        buff.next()
+        the_data = np.array([map(int, row.split(",")) for row in buff])
+    return the_data
 
-train_data = []
-for row in csv_train_file:
-    train_data.append(row)
+def train_and_predict():
+    train_data, test_data = map(read_file, [TRAINFILE,TESTFILE])
 
-test_data = []
-for row in csv_test_file:
-    test_data.append(row)
+    forest = RandomForestClassifier(n_estimators = NESTIMATORS).fit(train_data[:, 1:], train_data[:, 0])
 
-train_data = np.array(train_data)
-test_data = np.array(test_data)
+    output = Forest.predict(test_data)
 
-Forest = RandomForestClassifier(n_estimators = 100)
+    with open(OUTFILE, 'wb') as buff:
+        buff.write("ImageId,Label\n")
+        buff.write("\n".join(["{:d},{:d}".format(ndx+1,row) for ndx, row in enumerate(output)]))
 
-Forest = Forest.fit(train_data[0:, 1:], train_data[0:, 0])
-
-Output = Forest.predict(test_data[0:, :])
-
-outfile = csv.writer(open('predictions.csv', 'wb'))
-
-outfile.writerow(["ImageId", "Label"])
-
-for ndx, row in enumerate(Output):
-    outfile.writerow([ndx + 1, row])
+if __name__ == '__main__':
+    sys.exit(train_and_predict())
